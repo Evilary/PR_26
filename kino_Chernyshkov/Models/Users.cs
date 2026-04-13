8fbbd3146da85fd7545cdaf8128116aee126af97
df275 <df275@EVIL>
2026-04-13 11:22:34 +0500

    Разработанные модели. Чернышков.

---
+++ kino_Chernyshkov/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kino_Chernyshkov.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+
+        public DateTime RentStart { get; set; }
+
+        public int Duration { get; set; }
+
+        public int IdClub { get; set; }
+    }
+}
